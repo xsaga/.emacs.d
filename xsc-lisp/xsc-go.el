ac343f703a15dda30fec0c7551daa67ede9eab29
Xabier SdC <xabiersdc@gmail.com>
2024-09-02 16:03:45 +0200

    Actualizar configuración de Go.

--- xsc-lisp/xsc-go.el
+++ xsc-lisp/xsc-go.el
@@ -4,7 +4,23 @@
 
 ;;; Code:
 
-(xsc-install-packages '(go-mode))
+(xsc-install-packages '(go-mode
+			go-dlv))
+
+(add-hook 'go-mode-hook
+	  (lambda () (setq tab-width 4)))
+
+(add-hook 'go-ts-mode-hook
+	  (lambda () (setq tab-width 4)))
+
+(setq go-ts-mode-indent-offset 4)
+
+;; En windows "dlv debug" desde emacs da un error de que stdin is not a terminal
+(when (eq system-type 'windows-nt)
+  (defadvice dlv (before dlv-set-noninteractive-mswindows activate compile)
+    "Incluir por defecto el flag --allow-non-terminal-interactive=true"
+    (interactive
+     (list (read-from-minibuffer "Run dlv: " "dlv debug --allow-non-terminal-interactive=true")))))
 
 (provide 'xsc-go)
 ;;; xsc-go.el ends here
